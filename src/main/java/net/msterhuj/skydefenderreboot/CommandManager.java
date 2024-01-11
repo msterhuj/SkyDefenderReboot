f55ae460f532ed6c7c32b690bab412d1e9b4643c
msterhuj <gabin.lanore@gmail.com>
2024-01-11 16:00:56 +0100

    feat: rework Commands and Listener manager

--- src/main/java/net/msterhuj/skydefenderreboot/CommandManager.java
+++ src/main/java/net/msterhuj/skydefenderreboot/CommandManager.java
@@ -1,9 +1,42 @@
 package net.msterhuj.skydefenderreboot;
 
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+
+/**
+ * The {@code CommandManager} class is responsible for managing the registration of command executors
+ * for the SkyDefenderReboot plugin.
+ * <p>
+ * This class provides a way to associate command names with their corresponding command executors.
+ */
 public class CommandManager {
-    public CommandManager(SkyDefenderReboot skyDefenderReboot) {
-        skyDefenderReboot.getCommand("skydefenderreboot").setExecutor(new Commands());
-        skyDefenderReboot.getCommand("test").setExecutor(new TestCommands());
+
+    /**
+     * Constructs a new {@code CommandManager} and sets up command executors for the SkyDefenderReboot plugin.
+     *
+     * @param plugin The instance of the SkyDefenderReboot plugin.
+     */
+    public CommandManager(SkyDefenderReboot plugin) {
+        registerCommandExecutor(plugin, "skydefenderreboot", new Commands());
+        registerCommandExecutor(plugin, "test", new TestCommands());
+    }
+
+    /**
+     * Registers a command executor for the specified command.
+     *
+     * @param plugin   The instance of the plugin.
+     * @param command  The name of the command.
+     * @param executor The command executor associated with the command.
+     */
+    private void registerCommandExecutor(SkyDefenderReboot plugin, String command, CommandExecutor executor) {
+        PluginCommand pluginCommand = plugin.getCommand(command);
+        if (pluginCommand != null) {
+            pluginCommand.setExecutor(executor);
+        } else {
+            plugin.getLogger().warning("Failed to register command executor for '" + command + "'. Command not found.");
+        }
     }
 }
+
--- src/main/java/net/msterhuj/skydefenderreboot/ListenerManager.java
+++ src/main/java/net/msterhuj/skydefenderreboot/ListenerManager.java
@@ -4,12 +4,57 @@ import net.msterhuj.skydefenderreboot.core.GameListener;
 import net.msterhuj.skydefenderreboot.core.teams.TeamListener;
 import net.msterhuj.skydefenderreboot.core.teleporter.TeleporterListener;
 import net.msterhuj.skydefenderreboot.core.world.WorldListener;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 
+/**
+ * The {@code ListenerManager} class is responsible for managing the registration of event listeners
+ * for the SkyDefenderReboot plugin.
+ * <p>
+ * This class provides a flexible way to register and unregister multiple listeners with ease.
+ */
 public class ListenerManager {
-    public ListenerManager(SkyDefenderReboot skyDefenderReboot) {
-        skyDefenderReboot.getServer().getPluginManager().registerEvents(new TeleporterListener(), skyDefenderReboot);
-        skyDefenderReboot.getServer().getPluginManager().registerEvents(new TeamListener(), skyDefenderReboot);
-        skyDefenderReboot.getServer().getPluginManager().registerEvents(new GameListener(), skyDefenderReboot);
-        skyDefenderReboot.getServer().getPluginManager().registerEvents(new WorldListener(), skyDefenderReboot);
+
+    /**
+     * Constructs a new {@code ListenerManager} and registers default event listeners for the SkyDefenderReboot plugin.
+     *
+     * @param plugin The instance of the SkyDefenderReboot plugin.
+     */
+    public ListenerManager(SkyDefenderReboot plugin) {
+        registerListeners(plugin,
+                new TeleporterListener(),
+                new TeamListener(),
+                new GameListener(),
+                new WorldListener());
+    }
+
+    /**
+     * Registers multiple event listeners with the Bukkit PluginManager.
+     *
+     * @param plugin    The instance of the plugin.
+     * @param listeners The event listeners to be registered.
+     */
+    private void registerListeners(Plugin plugin, Listener... listeners) {
+        PluginManager pluginManager = plugin.getServer().getPluginManager();
+        for (Listener listener : listeners) {
+            pluginManager.registerEvents(listener, plugin);
+        }
+    }
+
+    /**
+     * Unregisters multiple event listeners.
+     * <p>
+     * Note: This method removes all registered handlers for the provided listeners.
+     *
+     * @param plugin    The instance of the plugin.
+     * @param listeners The event listeners to be unregistered.
+     */
+    private void unregisterListeners(Plugin plugin, Listener... listeners) {
+        PluginManager pluginManager = plugin.getServer().getPluginManager();
+        for (Listener listener : listeners) {
+            HandlerList.unregisterAll(listener);
+        }
     }
 }
